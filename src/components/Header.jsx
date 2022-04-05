8e423ee37875cd7b1f819a9924752241b2a1b49d
jejorm <jejorm8@gmail.com>
2022-04-04 20:39:04 -0500

    Title change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export const Header = () => {
     return (
         <>
             <h1 className='font-black text-5xl text-center mx-auto md:w-2/3'>
-                Seguimiento Pacientes {''}
+                Seguimiento Pacientes {''} con Github {''}
                 <span className='text-indigo-600'>Veterinaria</span>
             </h1>
         </>
